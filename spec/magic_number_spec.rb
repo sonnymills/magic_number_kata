d9d09e1dccb148ca34c02d0579d9a14fcff851ee
Emerson Mills <mrsn@odd-e.com>
2018-10-02 10:58:03 -0700

    initial setup for magic number kata

---
+++ spec/magic_number_spec.rb
@@ -0,0 +1,10 @@
+require_relative './spec_helper'
+require_relative '../magic_number_game'
+
+describe MagicNumberGame do 
+  it "can compile" do 
+      mng = MagicNumberGame.new 
+      expect(true).to be false 
+  end  
+
+end 
