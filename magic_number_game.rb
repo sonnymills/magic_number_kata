47cf1d8d2d61328c369ec6d0191458d2a893ecad
Aimee Furber <aimee@covered360.com>
2018-10-02 13:46:30 -0700

    yay mobbing\!

--- magic_number_game.rb
+++ magic_number_game.rb
@@ -1,3 +1,24 @@
 class MagicNumberGame 
-
+  attr_accessor :matrix, :col
+  def sum(row_index)
+      @matrix[row_index].reduce(:+)
+  end
+  def add_column(col_index)
+    @matrix.flatten.reduce(:+)
+  end
+  def add_diag(direction)
+    get_diag(direction).reduce(:+)
+  end
+  def get_diag(direction)
+    i = 0
+    diag = 0 .. 2
+    puts "THIS IS DIAG #{diag}"
+    diag.reverse! if direction == :right
+    diagonal = Array.new
+    @matrix.each do |c|
+      diagonal.push(c[i])
+      i += 1
+    end
+    return diagonal 
+  end
 end 
--- spec/magic_number_spec.rb
+++ spec/magic_number_spec.rb
@@ -2,9 +2,41 @@ require_relative './spec_helper'
 require_relative '../magic_number_game'
 
 describe MagicNumberGame do 
-  it "can compile" do 
-      mng = MagicNumberGame.new 
-      expect(true).to be false 
-  end  
+  before do 
+     @mng = MagicNumberGame.new 
+  end
+  it "can have a value in a cell" do
+     @mng.matrix = 1
+     expect(@mng.matrix).to eq 1
+  end
+  it "can add a row together" do 
+     @mng.matrix = [[1,2]]
+     expect(@mng.sum(0)).to eq 3
+  end
+  it "can have more than 1 row" do
+    @mng.matrix = [[], []] 
+    expect(@mng.matrix.length).to eq 2
+  end
+  it "can have more than 1 column" do
+    @mng.col = 3 
+    expect(@mng.col).to eq 3
+  end
+  it "can add two matrixs in a column together" do 
+    @mng.matrix = [[1], [2]] 
+    expect(@mng.add_column(0)).to eq 3
+  end
+  it "can get a diagonal" do
+    @mng.matrix = [[1,3],[3,2]]
+    diag = @mng.get_diag(:left)
+    expect(diag).to match_array([1,2])
+  end
 
+  it "can add two cells in the left diag together" do 
+    @mng.matrix = [[1,2],[1,2]]
+    expect(@mng.add_diag(:left)).to eq 3
+  end 
+  it "can add two cells in the right diag together" do 
+    @mng.matrix = [[1,3],[1,2]]
+    expect(@mng.add_diag(:right)).to eq 4
+  end
 end 
