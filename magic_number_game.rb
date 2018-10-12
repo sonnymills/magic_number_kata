bb0d89a910e52fd9acdd345b7b3f2ef91ae1f5b4
Emerson Mills <mrsn@odd-e.com>
2018-10-12 08:47:40 -0700

    latest fun times

--- magic_number_game.rb
+++ magic_number_game.rb
@@ -1,3 +1,53 @@
 class MagicNumberGame 
 
+  def set_matrix(matrix)
+    @matrix = matrix    
+  end
+
+  def get_matrix
+    return @matrix
+  end
+  def add_row(index)
+      return add_set(@matrix[index])
+  end 
+  def get_column(index)
+      column = Array.new
+      @matrix.each do  |r| 
+        column.push(r[index])
+      end
+      return column
+  end
+  def add_column(index)
+      return add_set(get_column(index))
+  end
+  def get_diagonal(diagonal_name)
+      diagonal = Array.new
+      max_element_index = (@matrix.length) -1
+      elements = (0 .. max_element_index ).to_a
+      if diagonal_name == :right
+        elements.reverse! 
+      end
+     @matrix.each_index do |i|
+        diagonal.push(@matrix[i][elements[i]])
+      end 
+      return diagonal 
+  end
+  def add_diagonal(diagonal_name)
+      return add_set(get_diagonal(diagonal_name))
+  end
+  def add_set(set)
+      return set.reduce(:+)
+  end 
+  def matrix_full?
+    @matrix.map{|r| r.length}.reduce(:+) == 9 ? true : false
+  end 
+  def find_the_number
+      magic_number = nil
+      (0..2).each do |i|
+        magic_number = add_column(i)  
+        raise "doesn't match" unless add_row(i) == magic_number
+      end
+      return magic_number 
+  end 
+
 end 
--- spec/magic_number_spec.rb
+++ spec/magic_number_spec.rb
@@ -2,9 +2,71 @@ require_relative './spec_helper'
 require_relative '../magic_number_game'
 
 describe MagicNumberGame do 
-  it "can compile" do 
-      mng = MagicNumberGame.new 
-      expect(true).to be false 
+  before :each do
+      @mng = MagicNumberGame.new 
+     matrix = [[1,2],[3,4]]
+     @mng.set_matrix(matrix)
+  end
+  it "can have a row of multiple cells" do 
+      @mng.set_matrix([[2,1]]) 
+      expect(@mng.get_matrix.first.length).to eq 2 
+  end
+  it "can add cells in a row" do
+      @mng.set_matrix([[2,1]]) 
+      expect(@mng.add_row(0)).to eq 3
+  end
+  it "can add cells in the Nth row" do
+     expect(@mng.add_row(1)).to eq 7
+  end
+  it "can create a matrix with more than one row" do 
+     expect(@mng.get_matrix.length).to eq 2 
+  end 
+  it "can get a column" do 
+     expect(@mng.get_column(0)).to match_array([1,3]) 
   end  
-
+  it "can get the Nth column" do 
+     expect(@mng.get_column(1)).to match_array([2,4]) 
+  end  
+  it "can add the Nth column" do 
+     expect(@mng.add_column(1)).to eq 6
+  end  
+  it "can get a diagonal" do
+     expect(@mng.get_diagonal(:left)).to match_array([1,4]) 
+  end
+  it "can get a right diagonal" do
+     expect(@mng.get_diagonal(:right)).to match_array([2,3]) 
+  end
+  it "can get the sum of a diagonal" do
+     matrix = [[2,2],[3,4]]
+     @mng.set_matrix(matrix)
+     expect(@mng.add_diagonal(:left)).to eq 6
+     expect(@mng.add_diagonal(:right)).to eq 5
+  end
+  it "can check if the matrix is 3x3" do
+     matrix = [[1,2,3],[3,4,5],[4,5,6]]
+     @mng.set_matrix(matrix)
+     expect(@mng.matrix_full?).to be true
+  end
+  it "3x3 diagonal totals still work" do 
+     matrix = [[1,2,3],[3,4,5],[4,5,6]]
+     @mng.set_matrix(matrix)
+     expect(@mng.add_diagonal(:left)).to eq 11
+  end 
+  it "raises an exception when a column total doesn't match the previous column total" do 
+     matrix = [[1,2,3],[3,2,1],[2,3,1]]
+     @mng.set_matrix(matrix)
+     expect{@mng.find_the_number}.to raise_error("doesn't match")
+  end 
+  it "raises an exception when a row total doesn't match the previous row total" do
+     matrix = [ [1,2,3],
+                [2,3,2],
+                [3,1,1]]
+     @mng.set_matrix(matrix)
+     expect{@mng.find_the_number}.to raise_error("doesn't match")
+  end
+  it "doesn't raise an exception when the totals match the previous total" do
+     matrix = [[1,1,1],[1,1,1],[1,1,1]]
+     @mng.set_matrix(matrix)
+     expect(@mng.find_the_number).to eq 3
+  end
 end 
